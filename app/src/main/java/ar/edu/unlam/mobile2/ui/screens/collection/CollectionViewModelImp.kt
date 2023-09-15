ec0f124ca718cd55a78a2ce247f4b7aa7ba95336
Marcos A. Verdura <mav.unlam@gmail.com>
2023-09-15 17:12:04 -0300

    se usan solamente flows en CollectionScreen y en CollectionViewModelImp.

--- app/src/main/java/ar/edu/unlam/mobile2/ui/screens/collection/CollectionViewModelImp.kt
+++ app/src/main/java/ar/edu/unlam/mobile2/ui/screens/collection/CollectionViewModelImp.kt
@@ -1,35 +1,33 @@
 package ar.edu.unlam.mobile2.ui.screens.collection
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import ar.edu.unlam.mobile2.data.repository.IHeroRepository
-import ar.edu.unlam.mobile2.ui.utilities.HeroListState
+import ar.edu.unlam.mobile2.domain.hero.DataHero
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
 class CollectionViewModelImp @Inject constructor(private val repo: IHeroRepository):ViewModel() {
-    var heroList by mutableStateOf(HeroListState())
-        private set
+
+    private val _heroList = MutableStateFlow<List<DataHero>>(emptyList())
+    val heroList = _heroList.asStateFlow()
+
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading = _isLoading.asStateFlow()
 
     init {
         viewModelScope.launch {
-            heroList = heroList.copy(
-                isLoading = true
-            )
-            heroList = heroList.copy(
-                heroList = withContext(Dispatchers.IO) {
+            _isLoading.value = true
+            _heroList.value = withContext(Dispatchers.IO) {
                     repo.getAllHero()
-                },
-                isLoading = false
-            )
+                }
+            _isLoading.value = false
         }
     }
-
 }
